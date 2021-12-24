4ebee5e6dde3de3e8dcb2ffe5572b527716c7797
zhongch18 <zhongch18@midea.com>
2021-12-24 14:00:36 +0800

    000

--- TTSanLingShell/ViewController.swift
+++ TTSanLingShell/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hellow world")
+        print("commit 000")
         // Do any additional setup after loading the view.
     }
 
