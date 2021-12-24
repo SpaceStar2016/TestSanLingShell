8eb8bca91f2090938b864890ceac6bb9825e486e
zhongch18 <zhongch18@midea.com>
2021-12-24 15:18:58 +0800

    002

--- TTSanLingShell/ViewController.swift
+++ TTSanLingShell/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("Hellow world")
         print("commit 000")
         print("commit 001")
+        print("commit 002")
         // Do any additional setup after loading the view.
     }
 
